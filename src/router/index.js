b46471ca60b18a93ffe3a6c088d91fe47bbd9e10
JosephDeFacto <josip.mareljic@gmail.com>
2023-11-17 14:22:15 +0100

    Set corresponding routes.

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
+import ProductListView from '@/views/ProductListView.vue'
+import ProductDetailsView from '@/views/ProductDetailsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: '/products',
+      name: 'product-list',
+      component: ProductListView
+    },
+    {
+      path: '/product/:id',
+      name: 'product-details',
+      component: ProductDetailsView,
+      props: true
     },
     {
       path: '/about',
@@ -15,7 +23,7 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: AboutView
     }
   ]
 })
